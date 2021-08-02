d3d1c7a3151a9848fdf9beaecaf9ad12bacae65c
Alex Moreno <alex.moreno.costa@gmail.com>
2021-08-02 09:37:13 -0300

    Versão inicial do código

--- text-counter/src/main/java/dev/amcosta/App.java
+++ text-counter/src/main/java/dev/amcosta/App.java
@@ -1,9 +1,30 @@
 package dev.amcosta;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String text = "Alex Moreno da Costa";
+        Map<String, Integer> textCounter = new HashMap<>();
+
+        for (int i = 0; i < text.length(); i++) {
+            String character = String.valueOf(text.charAt(i)).toLowerCase();
+            if (character.equals(" ")) {
+                continue;
+            }
+
+            Integer counter = 0;
+            if (textCounter.get(character) != null) {
+                counter = textCounter.get(character);
+            }
+
+            counter++;
+            textCounter.put(character, counter);
+        }
+
+        textCounter.forEach((character, quantity) -> System.out.println(String.format("%s: %d", character, quantity)));
     }
 }
